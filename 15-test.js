643801265f0d895763e3c74be3695547f817df9e
Dario Milanović <da7ryo@gmail.com>
2025-11-15 09:59:07 +0100

    Updated training (callback functions)

--- 15-test.js
+++ 15-test.js
@@ -19,3 +19,36 @@ function greetIfAdult(friend) {
 }
 
 processFriends(friends, greetIfAdult);
+
+const customers = [
+  { name: "Ana", spent: 120 },
+  { name: "Marko", spent: 40 },
+  { name: "Ivana", spent: 200 },
+  { name: "Luka", spent: 15 },
+];
+
+function processCustomers(customers, callback) {
+  for (let i = 0; i < customers.length; i++) {
+    callback(customers[i]);
+  }
+}
+
+function rewardCustomer(customer) {
+  if (customer.spent > 100) {
+    console.log(`Congratulations ${customer.name}! You have won a discount`);
+  } else {
+    console.log(`${customer.name}, thank You for buy`);
+  }
+}
+
+processCustomers(customers, rewardCustomer);
+
+function addCity(city) {
+  customers.city = city;
+}
+
+addCity("Zenica");
+
+processCustomers(customers, addCity);
+
+console.log(customers);
